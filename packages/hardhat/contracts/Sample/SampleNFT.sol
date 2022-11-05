6d8561e0615a4478954419e88ea4b2a106dd38a6
Xinbo Tian <32756076+xinbotian@users.noreply.github.com>
2022-11-05 13:06:14 -0700

    Update SampleNFT.sol

--- packages/hardhat/contracts/Sample/SampleNFT.sol
+++ packages/hardhat/contracts/Sample/SampleNFT.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.3/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CarbonCredit is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Paramo is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     bool public saleIsActive = true;
-    string private _baseURI;
+    string private _baseURIextended;
     uint256 public immutable MAX_SUPPLY;
     uint256 public currentPrice;
 
@@ -19,27 +20,27 @@ contract CarbonCredit is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     constructor(string memory _name,
         string memory _symbol,
         string memory _uri,
-        uint256 limit,
         uint256 price,
         uint256 maxSupply
     ) payable ERC721(_name, _symbol) {
-        _baseURIextended = _uri;
+        _baseURIextended = _uri; 
         currentPrice = price;
         MAX_SUPPLY = maxSupply;
         }
 
     function mint(uint256 amount) external payable {
         uint256 ts = totalSupply();
-        uint256 minted = _numberMinted(msg.sender);
+        uint256 tokenId = _tokenIdCounter.current();
 
         require(saleIsActive, "Sale must be active to mint tokens");
         require(ts + amount <= MAX_SUPPLY, "Purchase would exceed max tokens");
-        require(
-            currentPrice * amount == msg.value,
-            "Value sent is not correct"
-        );
-
-        _safeMint(msg.sender, amount);
+        require(currentPrice * amount == msg.value,"Value sent is not correct");
+        for(uint i = 0; i < amount; ++i){
+        tokenId = _tokenIdCounter.current();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _baseURIextended);
+        _tokenIdCounter.increment();
+        }
     }
 
     // The following functions are overrides required by Solidity.
@@ -73,4 +74,21 @@ contract CarbonCredit is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     function setCurrentPrice(uint256 price) external onlyOwner {
         currentPrice = price;
     }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
+
